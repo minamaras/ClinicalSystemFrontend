485c076e8e6434ab4555db447fc40db0cfe558e1
Mina Maras <40916170+minamaras@users.noreply.github.com>
2019-11-15 16:39:14 +0100

    Added Navbar for Patient

--- src/App.js
+++ src/App.js
@@ -7,12 +7,25 @@ export default class App extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      role: "",
+      isLoggedIn: false
+    }
+
   }
 
   render() { 
+
+    let role = this.state.role;
+    let isLoggedIn = this.state.isLoggedIn;
+    //this.setState((state) => ({ role: "patient", isLoggedIn: true}));
     return (
       <div className="App container">
-        <MyNavbar/>
+        <MyNavbar 
+          role = {role}
+          isLoggedIn = {isLoggedIn}
+        />
       </div>
 
     );
--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -8,26 +8,54 @@ import { LinkContainer } from "react-router-bootstrap";
 
 class MyNavbar extends React.Component {
 
-    render() {
-        return (
-            <Router>
-                <div className = "Mynavbar container">
-                <Navbar bg="light" variant="light" expand="lg">
-                    <Navbar.Brand>Clinical System</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/login">Login</Nav.Link>
-                <Nav.Link href="/register">Sign Up</Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-            </Navbar>   
-            </div>
-            </Router>
-            
+    constructor(props) {
+        super(props);
+    }
 
-        );
+    render() {
+
+        
+        if(!this.props.isLoggedIn) {
+            return (
+                <Router>
+                    <div className = "Mynavbar container">
+                    <Navbar bg="light" variant="light" expand="lg">
+                        <Navbar.Brand>Clinical System</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/login">Login</Nav.Link>
+                    <Nav.Link href="/register">Sign Up</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Navbar>   
+                </div>
+                </Router>
+                
+    
+            );
+        } else if(this.props.role === 'patient' && this.props.isLoggedIn) {
+            return (
+                <Router>
+                    <div className = "Mynavbar container">
+                    <Navbar bg="light" variant="light" expand="lg">
+                        <Navbar.Brand>Clinical System</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/exams">Exams</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Navbar>   
+                </div>
+                </Router>
+
+            );
+            
+        }
+       
     }
 
 }
