f510c979abd9b1e77747267cad3433175ae9f72d
tjncc <55355686+tjncc@users.noreply.github.com>
2019-12-01 01:49:25 +0100

    created page for choosing an admin

---
+++ src/components/AddAdminToClinic.js
@@ -0,0 +1,123 @@
+import React from 'react'
+import ClinicAdminForm from './ClinicAdminForm'
+import axios from 'axios';
+import { Modal, Button, ListGroup } from "react-bootstrap";
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
+const AdminConnectedAlert = withReactContent(Swal)
+
+class ClinicAdminTable extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+
+        this.state = {
+            show: false,
+            clinicadmins: [],
+            clinicid: props.id
+        }
+
+        //this.renderData = this.renderData.bind(this);
+
+        axios.get("http://localhost:8081/api/clinicadmin/available").then(
+            (resp) => this.onSuccessHandlerClinicAdmin(resp),
+            (resp) => this.onErrorHandlerClinicAdmin(resp)
+        );
+    }
+
+    handleClose() {
+        this.setState({ show: false });
+    }
+
+    handleShow() {
+        this.setState({ show: true });
+    }
+
+    onSuccessHandlerClinicAdmin(resp) {
+        var tempClinicAdmins = [];
+
+        for (var i = 0; i < resp.data.length; i++) {
+            tempClinicAdmins.push(resp.data[i]);
+        }
+        this.setState({
+            clinicadmins: tempClinicAdmins
+        });
+    }
+
+    onErrorHandlerClinicAdmin(response) {
+        alert("Error response: Uncovered case");
+    }
+
+    renderData(){
+        return this.state.clinicadmins.map((clinicadmin, index) => {
+            const { name, lastname, email, password} = clinicadmin
+
+            return(
+                <ListGroup key="email.toString()">
+                    <ListGroup.Item onClick={this.adminClicked.bind(this, clinicadmin)}>{name}, {email}</ListGroup.Item>
+                </ListGroup>
+
+            )
+        })
+    }
+    
+
+    render() {
+        return (
+            <div>
+            <Button id="adminadding" onClick={this.handleShow}>
+                    Choose Admin
+                </Button>
+                <Modal
+                    show={this.state.show}
+                    onHide={this.handleClose}
+                    size="lg"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered = "true"
+                >
+                <Modal.Header closeButton>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                        Available admins
+                        </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {this.renderData()}
+                    
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="success">Add</Button>
+                    <Button variant="secondary" onClick={this.handleClose}>Close</Button>
+                </Modal.Footer>
+                </Modal>
+                </div>
+        );
+    }
+
+    adminClicked(clinicadmin){
+        axios.post("http://localhost:8081/api/clinics/connectadmin",this.state.clinicid, clinicadmin).then(
+            console.log(clinicadmin),
+            console.log(this.state.clinicid),
+            (resp) => this.onSuccessHandler(resp),
+            (resp) => this.onErrorHandler(resp)
+          );
+
+    }
+
+    onErrorHandler(resp) {
+        alert("Error response: Uncovered case");
+    }
+
+    onSuccessHandler(resp){
+        window.location.reload();
+    }
+
+
+
+
+
+}
+
+export default ClinicAdminTable
