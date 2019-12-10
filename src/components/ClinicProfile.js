532cdc518439a61b03a3c1a60379ef84298b2f66
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2019-12-11 00:05:16 +0100

    Update ClinicProfile.js

--- src/components/ClinicProfile.js
+++ src/components/ClinicProfile.js
@@ -1,12 +1,12 @@
 import React from "react";
 import axios from 'axios';
-import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { Form, Button, FormGroup, FormControl, ControlLabel,Card } from "react-bootstrap";
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 import { withRouter,useParams} from "react-router-dom";
 import { Redirect } from 'react-router-dom'
 import ClinicListTable from './ClinicListTable'
-import '../css/ClinicListPage.css'
+import '../css/ClinicInfoPage.css'
 
 
 
@@ -62,9 +62,39 @@ class  ClinicProfile extends React.Component{
 
       render() {
           return (
-              <div>
-            
-              </div>
+            <Card className="text-center-clinic" id="karta">
+            <Form className="clinicInfo">
+
+            <Card.Body>
+              <h1 className="clinicHeader">Clinic's info</h1>
+              <br/>
+
+              <b><h3 className="clinicTitle">{this.state.clinicname}</h3></b>
+                <br/>
+                 <Form.Row>
+
+                    </Form.Row>
+
+                    <div className="clinicCol">
+
+                      <Form.Group className="firstColClinic" >
+                      <p className="valueNameClinic">Description: </p>
+                      <p className="valueNameClinic">Adress: </p>
+                      </Form.Group>
+
+                      <Form.Group className="secondColClinic" >
+                      <Form.Label className="inputDes">{this.state.description}</Form.Label>
+                      <Form.Label className="inputDes">{this.state.adress}</Form.Label>
+                      </Form.Group>
+
+                    </div>
+          </Card.Body>
+
+
+
+
+              </Form>
+              </Card>
           );
       }
 
