57a202343b03c55b0336718b2a45bb1ebf4c4779
Mina Maras <40916170+minamaras@users.noreply.github.com>
2019-12-22 22:17:01 +0100

    Predefined appointment form almost done

--- src/components/AddPredefinedAppointment.js
+++ src/components/AddPredefinedAppointment.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button,  DropdownButton,Dropdown} from "react-bootstrap";
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 import axios from 'axios'
 import DatePicker from 'react-date-picker'
 import TimePicker from 'react-time-picker'
+import Select from 'react-select';
 
 const RoomCreatedAlert = withReactContent(Swal)
 
@@ -22,6 +23,7 @@ class AddPredefinedAppointment extends React.Component {
         this.handleChange = this.handleChange.bind(this);
         this.onChange = this.onChange.bind(this);
         this.onChangeTime = this.onChangeTime.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
 
         this.addPredefinedAppointment = this.addPredefinedAppointment.bind(this);
 
@@ -29,14 +31,110 @@ class AddPredefinedAppointment extends React.Component {
             show: false,
             duration: '',
             doctor: '',
+            doctors: [],
             room: '',
+            rooms: [],
             type: '',
+            examtypes: [],
             examTime: '10:00',
 
             examDate: date,
         };
 
-        console.log(this.props.tipovi);
+    }
+
+    componentDidMount() {
+        let token = localStorage.getItem('token');
+        const options = {
+            headers: { 'Authorization': 'Bearer ' + token}
+            };
+
+        axios.get('http://localhost:8081/api/examtypes/all', options).then(
+            (resp) => this.onSuccessHandlerExam(resp),
+            (resp) => this.onErrorHandlerExam(resp),
+        );
+
+        axios.get('http://localhost:8081/api/rooms/all', options).then(
+            (resp) => this.onSuccessHandlerRoom(resp),
+            (resp) => this.onErrorHandlerRoom(resp),
+        );
+
+        axios.get('http://localhost:8081/api/doctors/alldoctors', options).then(
+            (resp) => this.onSuccessHandlerDoctor(resp),
+            (resp) => this.onErrorHandlerDoctor(resp),
+        );
+    }
+
+    onSuccessHandlerExam(resp) {
+
+        var tempexams = [];
+      
+        console.log(resp.data);
+      
+        for (var i = 0; i < resp.data.length; i++) {
+            tempexams.push(resp.data[i].name);
+            console.log(resp.data[i]);
+            console.log(resp.data[i].name);
+            console.log(tempexams);
+        }
+      
+        this.setState({
+            examtypes : tempexams,
+        });
+      
+      
+    }
+
+    onSuccessHandlerDoctor(resp) {
+
+        var tempdoctors = [];
+      
+        console.log(resp.data);
+      
+        for (var i = 0; i < resp.data.length; i++) {
+            tempdoctors.push(resp.data[i].name);
+            console.log(resp.data[i]);
+            console.log(resp.data[i].name);
+        }
+      
+        this.setState({
+            doctors : tempdoctors,
+        });
+      
+      
+    }
+
+
+    onSuccessHandlerRoom(resp) {
+
+        var temprooms = [];
+      
+        console.log(resp.data);
+      
+        for (var i = 0; i < resp.data.length; i++) {
+            temprooms.push(resp.data[i].name);
+            console.log(resp.data[i]);
+            console.log(resp.data[i].name);
+        }
+      
+        this.setState({
+            rooms : temprooms,
+        });
+      
+      
+    }
+
+
+    onErrorHandlerExam(response) {
+        alert("Error response: Uncovered case");
+    }
+
+    onErrorHandlerDoctor(response) {
+        alert("Error response: Uncovered case");
+    }
+
+    onErrorHandlerRoom(response) {
+        alert("Error response: Uncovered case");
     }
 
     onChange = (date) => this.setState({ examDate: date })
@@ -93,6 +191,24 @@ class AddPredefinedAppointment extends React.Component {
         this.setState({...this.state, [e.target.name]: e.target.value});
     }
 
+    handleSelect(eventKey, event) {
+        event.preventDefault();
+
+        this.setState({ type: this.state.examtypes[eventKey] });
+    }
+
+    handleSelectDoctor(eventKey, event) {
+        event.preventDefault();
+
+        this.setState({ doctor: this.state.doctors[eventKey] });
+    }
+
+      handleSelectRoom(eventKey, event) {
+        event.preventDefault();
+
+        this.setState({ room: this.state.rooms[eventKey] });
+    }
+
     render() {
         return (
             <div>
@@ -115,42 +231,60 @@ class AddPredefinedAppointment extends React.Component {
                         <form onSubmit={this.addPredefinedAppointment} id="addDoctorForm">
                             <div className="form-group">
                                 <label htmlFor="doctor">Doctor</label>
-                                <select id="doctors" name="doctors" className="form-control form-control-sm" required>
-                                    <option value="CSHARP">.NET Core 2.1</option>
-                                    <option value="PYTHON">Python 3</option>
-                                    <option value="NODE">node.js 11</option>
-                                    <option value="GO">Go</option>
-                                </select>
-                                <br/>
+                                <DropdownButton
+                                    title={this.state.doctor}
+                                    id="document-type"
+                                    variant="info"
+                                    onSelect={this.handleSelectDoctor.bind(this)}
+                                >
+                                    {this.state.doctors.map((opt, i) => (
+                                        <Dropdown.Item key={i} eventKey={i}>
+                                            {opt}
+                                        </Dropdown.Item>
+                                    ))}
+                                </DropdownButton>
+                                <br />
                                 <label htmlFor="examDate">Date</label>
-                                <br/>
-                                <DatePicker id="examDate" name="examDate" onChange={this.onChange} value={this.state.examDate}/>
-                                <br/>
-                                <br/>
+                                <br />
+                                <DatePicker id="examDate" name="examDate" onChange={this.onChange} value={this.state.examDate} />
+                                <br />
+                                <br />
                                 <label htmlFor="examTime">Time</label>
-                                <br/>
-                                <TimePicker id="examTime" name="examTime" onChange={this.onChangeTime} value={this.state.examTime}/>
-                                <br/>
-                                <br/>
+                                <br />
+                                <TimePicker id="examTime" name="examTime" onChange={this.onChangeTime} value={this.state.examTime} />
+                                <br />
+                                <br />
                                 <label htmlFor="room">Room</label>
-                                <select id="doctors" name="doctors" className="form-control form-control-sm" required>
-                                    <option value="CSHARP">.NET Core 2.1</option>
-                                    <option value="PYTHON">Python 3</option>
-                                    <option value="NODE">node.js 11</option>
-                                    <option value="GO">Go</option>
-                                </select>
-                                <br/>
-                                <label htmlFor="type">Exam type</label>
-                                <select id="doctors" name="doctors" className="form-control form-control-sm" required>
-                                    <option value="CSHARP">.NET Core 2.1</option>
-                                    <option value="PYTHON">Python 3</option>
-                                    <option value="NODE">node.js 11</option>
-                                    <option value="GO">Go</option>
-                                </select>
+                                <DropdownButton
+                                    title={this.state.room}
+                                    id="document-type"
+                                    variant="info"
+                                    onSelect={this.handleSelectRoom.bind(this)}
+                                >
+                                    {this.state.rooms.map((opt, i) => (
+                                        <Dropdown.Item key={i} eventKey={i}>
+                                            {opt}
+                                        </Dropdown.Item>
+                                    ))}
+                                </DropdownButton>
+                                <br />
+                                Select exam type :{" "}
+                                <DropdownButton
+                                    title={this.state.type}
+                                    id="document-type"
+                                    variant="info"
+                                    onSelect={this.handleSelect.bind(this)}
+                                >
+                                    {this.state.examtypes.map((opt, i) => (
+                                        <Dropdown.Item key={i} eventKey={i}>
+                                            {opt}
+                                        </Dropdown.Item>
+                                    ))}
+                                </DropdownButton>
                             </div>
-                            <hr/>
-                            <Button className="roomDugme" variant = "secondary" style={{float: "right"}} onClick={this.handleClose}>Close</Button>
-                            <Button type="submit" variant = "success" style={{float: "right", margin: "0px 10px 0px 0px" }} className="roomDugme">Add</Button>
+                            <hr />
+                            <Button className="roomDugme" variant="secondary" style={{ float: "right" }} onClick={this.handleClose}>Close</Button>
+                            <Button type="submit" variant="success" style={{ float: "right", margin: "0px 10px 0px 0px" }} className="roomDugme">Add</Button>
                         </form>
                     </Modal.Body>
                 </Modal>
