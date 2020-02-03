fdfd0be48b921c3c5eb27176a301bf3e804b394f
tjncc <55355686+tjncc@users.noreply.github.com>
2020-02-03 03:35:31 +0100

    implemented doctor's calendar

--- src/components/MyCalendar.js
+++ src/components/MyCalendar.js
@@ -1,55 +1,230 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
+import  BigCalendar, {momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'moment/locale/en-gb';
-
+import axios from 'axios';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { withRouter } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import '../css/MyCalendar.css'
+import tipicon from '../icons/lamp.svg'
 
-
+const PatientAlert = withReactContent(Swal);
 const localizer = momentLocalizer(moment);
 
-const myEventsList = []
-
 class MyCalendar extends React.Component {
+
+  
+
   constructor() {
     
     super();
 
+    this.createEvents = this.createEvents.bind(this);
+    this.createHolidays = this.createHolidays.bind(this);
+    this.customEventPropGetter = this.customEventPropGetter.bind(this);
+    this.onSelectEvent = this.onSelectEvent.bind(this);
     
     const now = new Date();
-    const events = [
-      {
-        id: 14,
-        title: 'Today',
-        start: new Date(new Date().setHours(new Date().getHours() - 1)),
-        end: new Date(new Date().setHours(new Date().getHours() + 3)),
-      },
-      
-    ]
+
     this.state = {
       name: 'React',
-      events
+      startShift: new Date(),
+      endShift: new Date(),
+      doctor: {},
+      events: [],
+      appointments: [],
+      holidays: [],
     };
     
   }
 
   componentDidMount(){
+    
+    let token = localStorage.getItem('token');
+    const options = {
+      headers: { 'Authorization': 'Bearer ' + token}
+          };
+
+    axios.get('http://localhost:8081/api/doctors/getdates',options).then(
+              (resp) => this.onSuccessHandler(resp),
+              (resp) => this.onErrorHandler(resp)
+              );
+
 
   }
 
+  onErrorHandler(resp) {
+    PatientAlert.fire({
+        title: "Error occured",
+        text: '',
+        type: "error",
+        button: true
+      });
+  
+  }
+   
+  
+  onSuccessHandler(resp) {
+    console.log(resp.data)
+  
+
+    var splitedstart = resp.data.start.split(':');
+    var splitedend = resp.data.end.split(':');
+
+    var begin = new Date(0,0,0,splitedstart[0],splitedstart[1],splitedstart[2]);
+    var end = new Date(0,0,0,splitedend[0],splitedend[1],splitedend[2]);
+
+    this.setState({
+  
+      startShift : begin,
+      endShift : end,
+      appointments : resp.data.appointments,
+      holidays : resp.data.holidays,
+
+    })
+
+    this.createEvents();
+    this.createHolidays();
+  }
+
+
+  createEvents(){
+
+    var appointEvents = []
+
+    this.state.appointments.map((app, index) => {
+      if(app.classification !== "NORMAL"){
+        return;
+      }
+
+      var splitedstart = app.startTime.split(':');
+      var splitedend = app.endTime.split(':');
+      var spliteddate = app.date.split('-')
+     // var numapp = index + 1;
+      var beginApp = new Date(spliteddate[0],spliteddate[1]-1,spliteddate[2],splitedstart[0],splitedstart[1],splitedstart[2]);
+      var endApp = new Date(spliteddate[0],spliteddate[1]-1,spliteddate[2],splitedend[0],splitedend[1],splitedend[2]);
+
+
+    appointEvents.push({
+      title: app.examTypeName,
+      startDate: beginApp,
+      endDate: endApp,
+      allDay: false,
+      resource: app.id, 
+  
+    });
+  })
+
+  this.setState({
+    events: this.state.events.concat(appointEvents)
+})
+
+console.log(this.state.appointments);
+
+}
+
+
+
+
+
+createHolidays(){
+  var holidayEvents = []
+
+  this.state.holidays.map((holiday, index) => {
+
+    var starth = holiday.startHoliday.split('-');
+    var endh = holiday.endHoliday.split('-');
+    //var numapp = index + 1;
+
+    var beginH = new Date(starth[0],starth[1]-1,starth[2]);
+    var endH = new Date(endh[0],endh[1]-1,endh[2]);
+
+
+  holidayEvents.push({
+    title: 'Holiday',
+    startDate: beginH,
+    endDate: endH,
+    allDay: true,
+
+  });
+})
+
+
+this.setState({
+  events: this.state.events.concat(holidayEvents)
+})
+
+}
+
+customEventPropGetter(event){
+  //console.log(event)
+
+  if(event.title.includes('Holiday')){
+    return {
+      style: {
+        backgroundColor: '#4f4f4f',
+      },
+    }
+  } else {
+    return {
+      style: {
+        backgroundColor: '#82c2e0',
+        color: 'black',
+      }
+    }
+  
+} 
+}
+
+onSelectEvent(event){
+
+  this.state.appointments.forEach(element => {
+    if(element.id === event.resource){
+      if(!element.status.includes('HAS_HAPPEND')){
+        window.location.href=`http://localhost:3000/startappointment/medicalrecord/${event.resource}`
+      }
+     else if (element.status.includes('HAS_HAPPEND')) {
+      PatientAlert.fire({
+        title: "This appointment has already happened.",
+        text: '',
+        type: "error",
+        icon: 'error',
+        button: true
+      });
+    }
+  }
+
+  }); 
+
+}
+
+
   render() {
+    
     return (
       <div>
           <br />
-        <div style={{ height: '450pt', margin: '50px 0 0 0'}}>
-          <Calendar
+          <div className="calendarInfo">
+            <img style={{height:'55px', width: 'auto'}} src={tipicon} className="tipIcon" />
+          <p>You can start the appointment by clicking on it.</p>
+          </div>
+
+        <div  style={{ height: '470pt', margin: '40px 0 0 0'}} >
+          
+          <BigCalendar
             localizer={localizer}
-            events={myEventsList}
-            startAccessor="start"
-            endAccessor="end"
-            defaultDate={moment().toDate()}
+            events={this.state.events}
+            views={['month', 'week', 'day']}
+            startAccessor="startDate"
+            endAccessor="endDate"
+            min={this.state.startShift}
+            max={this.state.endShift}
+            eventPropGetter={this.customEventPropGetter}
+            onSelectEvent={this.onSelectEvent}
           />
         </div>
       </div>
